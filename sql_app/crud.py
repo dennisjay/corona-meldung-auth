b701d5fd0f26273e098e85c330412e2caee299e6
floanwelt1 <floanwelt@gmail.com>
2020-03-26 22:52:24 +0100

    Removed password from /users/ endpoint

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -16,13 +16,11 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
-    from sql_app.auth import get_password_hash
-    hashed_password = get_password_hash(user.password)
+def create_user(db: Session, user: schemas.UserBase):
     key = jwk.JWK.generate(kty='oct', size=256)
     key_json_str = key.export()
 
-    db_user = models.User(email=user.email, hashed_password=hashed_password, jwk_key=key_json_str)
+    db_user = models.User(email=user.email, jwk_key=key_json_str)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
--- sql_app/main.py
+++ sql_app/main.py
@@ -50,7 +50,7 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
 
 
 @app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
