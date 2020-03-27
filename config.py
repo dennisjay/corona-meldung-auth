ae0084696c34ecf5ad390ba43d62b277a4dfec4c
dj <dj@dennis-joest.de>
2020-03-27 17:47:34 +0100

    Mysql

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_ADDRESS = 'register@corona-meldung.de'
 EMAIL_SMTP_SERVER = 'smtp.ionos.com'
 EMAIL_SMTP_PASSWORD = os.environ.get('EMAIL_SMTP_PASSWORD')
+MYSQL_CONNECTION_URL = os.environ.get('MYSQL_CONNECTION_URL')
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 300
