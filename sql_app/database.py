66aeb1ff49fda5788c03e4855600ec61d5bb5efc
Dennis Jöst <djprivat@googlemail.com>
2020-03-27 18:24:52 +0100

    Mysql

--- sql_app/database.py
+++ sql_app/database.py
@@ -5,9 +5,7 @@ import config
 
 SQLALCHEMY_DATABASE_URL = config.MYSQL_CONNECTION_URL
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
