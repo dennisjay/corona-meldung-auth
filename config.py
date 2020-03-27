c645278ff5634187575a14a06ac6e9d5d70193c1
Dennis Jöst <djprivat@googlemail.com>
2020-03-27 14:50:50 +0100

    Mail sending test

--- config.py
+++ config.py
@@ -2,4 +2,8 @@ import os
 
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
+EMAIL_ADDRESS = 'register@corona-meldung.de'
+EMAIL_SMTP_SERVER = 'smtp.ionos.com'
+EMAIL_SMTP_PASSWORD = os.environ.get('EMAIL_SMTP_PASSWORD')
+
 ACCESS_TOKEN_EXPIRE_MINUTES = 300
