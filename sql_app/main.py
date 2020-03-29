451655f1557910ae620c57040eb4188ff115dd9e
dj <dj@dennis-joest.de>
2020-03-29 15:52:40 +0200

    Chnage status code and remove unnessary endpoints

--- sql_app/main.py
+++ sql_app/main.py
@@ -16,7 +16,6 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-
 origins = [
     "http://localhost",
     "http://localhost:8000",
@@ -34,10 +33,10 @@ app.add_middleware(
 
 @app.post("/register", response_model=schemas.UserBase)
 def register_user(user: schemas.UserBase, db: Session = Depends(get_db)):
-
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        # Status Code 409 for Conflict
+        raise HTTPException(status_code=409, detail="Email already registered")
     db_user = crud.create_user(db=db, user=user)
 
     # Sent Email with activation key to use in confirm_user()
@@ -103,36 +102,3 @@ def confirm_login(user: schemas.UserLogin, db: Session = Depends(get_db)):
         return schemas.User(id=db_user.id, email=db_user.email, is_active=db_user.is_active, jwk_key=db_user.jwk_key)
     else:
         raise HTTPException(status_code=400, detail="Could not activate user")
-
-
-@app.post("/token", response_model=schemas.Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-
-
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserBase, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-    db_user = crud.create_user(db=db, user=user)
-    return schemas.User(id=db_user.id, email=db_user.email, is_active=db_user.is_active, jwk_key=db_user.jwk_key)
-
-
-@app.get("/users/me/", response_model=schemas.User)
-async def read_users_me(current_user: schemas.User = Depends(get_current_active_user)):
-    return schemas.User(id=current_user.id, email=current_user.email, is_active=current_user.is_active, jwk_key=current_user.jwk_key)
-
-
