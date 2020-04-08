69e6061090db744585659497e3945bcd4996a4fc
dj <dj@dennis-joest.de>
2020-04-08 05:10:35 +0200

    scoped session to avoid mysql timeouts

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 import config
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///test.db'
@@ -14,7 +14,7 @@ else:
     print("Using SQLLITE")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
 
